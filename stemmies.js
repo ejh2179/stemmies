43524cb1b12639bdb5931957381f71ee7a8d5c43
stephanieyang <stephanie.yang.121@gmail.com>
2016-03-06 21:48:26 -0500

    some modification of revert option

--- stemmies.js
+++ stemmies.js
@@ -9,24 +9,28 @@ $(document).ready(function() {
 			console.log("event",event);
 			console.log("ui",ui);
 			console.log(ui.draggable.context.id);
-			items.push(ui.draggable.context.id);
-			$.unique(items);
+			items.push(ui.draggable.context.id); // add new item to list
+			$.unique(items); // get rid of duplicates
 			console.log(items);
-            handleDroppable(); // color background accordingly
-        }
+			handleDroppable(); // color background accordingly
+		}
 	});
 
 	$('.image').draggable({
 		//stack: event.target.id,
 		cursor: 'hand',
-		revert: 'invalid'
+		revert: 'invalid',
+		stop: function(event,ui) {
+			console.log("event",event);
+			console.log("ui",ui);
+		}
 	});
-
+/*
 	$('#test').draggable({
 		//stack: event.target.id,
 		revert: 'valid'
 	});
-
+*/
 	$("#check").click(function() {
 		console.log($("#laboratory .image").length);
 	});
@@ -39,6 +43,35 @@ $(document).ready(function() {
 			$("#laboratory").droppable("disable");
 			$("#laboratory").css('background-color','#C0C0C0');
 		}
+		handleImageReverts();
+	}
+
+	function isInDropZone(idName) {
+		for(var i = 0; i < items.length; i++) {
+			if(items[i] === idName) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/*
+	 * STEPH-ONLY NOTES: this is O(n*k), where k = max number of items allowable in drop zone.
+	 * How can this be improved?
+	 */
+	function handleImageReverts() {
+		var imageList = $('.image');
+		for(var i = 0; i < imageList.length; i++) {
+			if(isInDropZone($(imageList[i]).attr('id'))) {
+				console.log($(imageList[i]).attr('id'));
+				$(imageList[i]).draggable( "option", "revert", false );
+			} 
+			/*
+			else {
+				$(imageList[i]).draggable( "option", "revert", "invalid" );
+			}
+			*/
+		}		
 	}
 
 	/*
